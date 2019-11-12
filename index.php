2d2804ad9ed358e6eb0eacece08845fd6185b1b4
Paulo Alves <front@agencianovi.com.br>
2019-11-12 00:57:19 -0200

    a

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php include 'components/header.php'; ?>
+
+	<section class="vitrine">
+		<div class="owl-carousel owl-theme owl-vitrine">
+			<div class="item">
+				<a href=""><img src="assets/images/vitrine.jpg" alt=""></a>
+			</div><!-- item -->
+		</div><!-- owl-carousel -->
+	</section><!-- vitrine -->
+
+	<section class="pilares">
+		<div class="container">
+			<h1 class="text-center">PILARES DA PRÉ-ESCOLA PRIMEIRA INFÂNCIA</h1>
+			<p class="text-center"><strong>A gente sabe: escolher o lugar onde o seu filho ou a sua filha passará a primeira infância não é uma tarefa fácil!</strong> Em nosso site você terá acesso a informações completas sobre Primeira Infância, mas se o tempo estiver curto, gostaríamos que rapidamente você gravasse quais são os pilares que sustentam a nossa escola.</p>
+			<div class="row">
+				<div class="col-md-6 col-sm-6">
+					<img src="assets/images/icone1.png">
+					<h2>CRIANÇA</h2>
+					<p></p>
+				</div><!-- md-6 -->
+				<div class="col-md-6 col-sm-6">
+					<img src="assets/images/icone1.png">
+					<h2>CRIANÇA</h2>
+					<p></p>
+				</div><!-- md-6 -->
+				<div class="col-md-6 col-sm-6">
+					<img src="assets/images/icone1.png">
+					<h2>CRIANÇA</h2>
+					<p></p>
+				</div><!-- md-6 -->
+				<div class="col-md-6 col-sm-6">
+					<img src="assets/images/icone1.png">
+					<h2>CRIANÇA</h2>
+					<p></p>
+				</div><!-- md-6 -->
+			</div><!-- row -->
+		</div><!-- container -->
+	</section><!-- pilares -->
+	
+<?php include 'components/footer.php'; ?>
