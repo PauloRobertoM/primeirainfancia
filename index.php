2fde475fcb9db09b7dfd09d373a35a4fd42c847c
Paulo Alves <front@agencianovi.com.br>
2019-11-13 00:36:38 -0200

    avnaçando

--- index.php
+++ index.php
@@ -14,27 +14,70 @@
 			<p class="text-center"><strong>A gente sabe: escolher o lugar onde o seu filho ou a sua filha passará a primeira infância não é uma tarefa fácil!</strong> Em nosso site você terá acesso a informações completas sobre Primeira Infância, mas se o tempo estiver curto, gostaríamos que rapidamente você gravasse quais são os pilares que sustentam a nossa escola.</p>
 			<div class="row">
 				<div class="col-md-6 col-sm-6">
-					<img src="assets/images/icone1.png">
-					<h2>CRIANÇA</h2>
-					<p></p>
+					<div class="item item1">
+						<img src="assets/images/item1.png">
+						<div class="content">
+							<h2>CRIANÇA</h2>
+							<p>dasda sdasdasd asdas das dasdasda.</p>
+						</div><!-- content -->
+					</div><!-- item -->
 				</div><!-- md-6 -->
 				<div class="col-md-6 col-sm-6">
-					<img src="assets/images/icone1.png">
-					<h2>CRIANÇA</h2>
-					<p></p>
+					<div class="item item2">
+						<img src="assets/images/item2.png">
+						<div class="content">
+							<h2>CRIANÇA</h2>
+							<p>dasda sdasdasd asdas das dasdasda.</p>
+						</div><!-- content -->
+					</div><!-- item -->
 				</div><!-- md-6 -->
 				<div class="col-md-6 col-sm-6">
-					<img src="assets/images/icone1.png">
-					<h2>CRIANÇA</h2>
-					<p></p>
+					<div class="item item3">
+						<img src="assets/images/item3.png">
+						<div class="content">
+							<h2>CRIANÇA</h2>
+							<p>dasda sdasdasd asdas das dasdasda.</p>
+						</div><!-- content -->
+					</div><!-- item -->
 				</div><!-- md-6 -->
 				<div class="col-md-6 col-sm-6">
-					<img src="assets/images/icone1.png">
-					<h2>CRIANÇA</h2>
-					<p></p>
+					<div class="item item4">
+						<img src="assets/images/item4.png">
+						<div class="content">
+							<h2>CRIANÇA</h2>
+							<p>dasda sdasdasd asdas das dasdasda.</p>
+						</div><!-- content -->
+					</div><!-- item -->
 				</div><!-- md-6 -->
 			</div><!-- row -->
 		</div><!-- container -->
 	</section><!-- pilares -->
+
+	<section class="download">
+		<div class="container">
+			<a href=""><img src="assets/images/corpo-download.png" class="img-responsive"></a>
+		</div><!-- container -->
+	</section><!-- download -->
+
+	<section class="minha-casa">
+		<div class="container">
+			<h1 class="text-center">MINHA SEGUNDA CASA</h1>
+			<div class="row">
+				<div class="col-md-6 col-sm-6">
+					<img src="assets/images/minha-casa.jpg" class="img-responsive">
+				</div><!-- md-6 -->
+				<div class="col-md-6 col-sm-6">
+					<p>a kjsn dasda sdas das d <strong>dkasjn</strong> dakj ndaksjdn ask jndask jdna.</p>
+				</div><!-- md-6 -->
+			</div><!-- row -->
+			<a href="">CONFIRA TODAS AS NOSSAS ATIVIDADES</a>
+		</div><!-- container -->
+	</section><!-- minha-casa -->
+
+	<section class="minha-casa">
+		<div class="container">
+			<h1 class="text-center">MINHA SEGUNDA CASA</h1>
+		</div><!-- container -->
+	</section><!-- minha-casa -->
 	
 <?php include 'components/footer.php'; ?>
